87e5dc2c2fdce6cd2634fa7a7c8e4620613b0554
caimyuz <7115944@gmail.com>
2026-09-04 00:39:03 +0300

    Refactor Yemot IVR Flask service code

--- app.py
+++ app.py
@@ -58,7 +58,7 @@ def yemot_ivr():
         try:
             payload = {
                 "token": f"{system_number}:{password}",
-                "path": "ivr2:/ext",
+                "path": "ivr2:/",
                 "type": "nitoviya",
                 "nitoviya_dial_to": ROUTING_NUMBER,
             }
